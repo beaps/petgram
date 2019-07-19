814c8116f79187df6475bfbd6651c2302a98c5ef
Beaps <beapsant@gmail.com>
2019-07-19 21:25:16 +0100

    Create PhotoCard and use react-icon

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -10,20 +10,9 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: inherit;
   }
 
-  ul, li, h1, h2, h3, p, button {
-    margin: 0;
-    padding: 0;
-  }
-
-  ul {
-    list-style: none;
-  }
-
-  button: {
-    background: transparent;
-    border: 0;
-    outline: 0;
-  }
+  ul, li, h1, h2, h3, p, button { margin: 0; padding: 0; }
+  ul { list-style: none; }
+  button { background: transparent; border: 0; outline: 0 }
 
   body {
     background: #fefefe;
