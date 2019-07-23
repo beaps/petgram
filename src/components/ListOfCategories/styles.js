57bdbe7c3ee9bab45c7bca9f99d8257e9b6e20c7
Beaps <beapsant@gmail.com>
2019-07-23 12:10:54 +0100

    Create a custom hook

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { fadeIn } from '../../styles/animation'
 
 export const List = styled.ul`
@@ -6,7 +6,8 @@ export const List = styled.ul`
   overflow: scroll;
   width: 100%;
   margin-bottom: 1.5rem;
-  &.fixed {
+  ${props => props.fixed && css`
+  {
     ${fadeIn({ time: '1.3s' })}
     background: #fff;
     border-radius: 60px;
@@ -21,6 +22,7 @@ export const List = styled.ul`
     transform: scale(0.5);
     z-index: 1;
   }
+  `}
 `
 
 export const Item = styled.li`
