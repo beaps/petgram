4a3953b68eaa04e337476343dab073048b865d3f
Beaps <beapsant@gmail.com>
2019-07-25 19:28:54 +0100

    Add Intersection Observer to PhotoCard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,17 +1,36 @@
-import React from 'react'
-import { ImgWrapper, Img, Button, SvgContainer, LikesContainer } from './styles'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Img, Button, SvgContainer, LikesContainer } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log('si')
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+    <Article ref={element}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+        </Fragment>
+      }
 
       <Button>
         <SvgContainer>
@@ -21,6 +40,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           {likes} likes
         </LikesContainer>
       </Button>
-    </article>
+    </Article>
   )
 }
