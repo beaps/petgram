0a1bde13b7eaea645a340b9fc4f93dcd9a809526
Beaps <beapsant@gmail.com>
2019-07-30 20:17:05 +0100

    Use Mutation to register users

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -6,10 +6,18 @@ export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({
+      email: email.value,
+      password: password.value
+    })
+  }
+
   return (
     <Fragment>
       <Title>{title}</Title>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Input placeholder='email' type='email' {...email} />
         <Input placeholder='password' type='password' {...password} />
         <Button>{title}</Button>
