6c29d76c4e6ca75cc8c6389300f385369de14332
Beaps <beapsant@gmail.com>
2019-08-01 21:08:31 +0100

    Use PropTypes

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, SvgContainer, LikesContainer } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { PropTypes } from 'prop-types'
 
 export const FavButton = ({ liked, likes, onClick }) => {
   const Icon = liked ? MdFavorite : MdFavoriteBorder
@@ -15,4 +16,10 @@ export const FavButton = ({ liked, likes, onClick }) => {
       </LikesContainer>
     </Button>
   )
+}
+
+FavButton.propTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
 }
