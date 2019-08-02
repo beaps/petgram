d03729af3ff93dadef4e6cbba95ffa41530559fa
Beaps <beapsant@gmail.com>
2019-08-02 14:11:10 +0100

    Add Service Worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
 const path = require('path')
 
 module.exports = {
@@ -16,11 +18,29 @@ module.exports = {
       shortname: 'Petgram 🐶',
       description: 'Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente',
       background_color: '#fff',
-      theme_color: '#b1a',
+      theme_color: '#70ee8e',
       icons: [
         {
           src: path.resolve('src/assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-bea.beaps.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
